961e397a80b74485d0f81fdc3c0099e7d98019a5
Hamza Zaheer <malikhamza1619619@gmail.com>
2022-09-08 12:23:34 +0500

    commit 23 spinner testing

--- src/shared/Homepage/Homepage.jsx
+++ src/shared/Homepage/Homepage.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import TopDonations from "./TopDonations/TopDonations";
 import TopDonors from "./TopDonors/TopDonors";
+
 import "./Homepage.css";
 
 const Homepage = () => {
   return (
     <React.Fragment>
-      <div className="banner">
-        <div>
-          <h1>Welcome to the Place of Kindness</h1>
-          <h5>
-            Its not how much we give, but how much love we put into giving.
-          </h5>
-          <h4>Mother Teresa</h4>
+      <div>
+        <div className="banner">
+          <div>
+            <h1>Welcome to the Place of Kindness</h1>
+            <h5>
+              Its not how much we give, but how much love we put into giving.
+            </h5>
+            <h4>Mother Teresa</h4>
+          </div>
         </div>
-      </div>
 
-      <div>
-        <TopDonors />
-      </div>
-      <div>
-        <TopDonations />
-        <br />
+        <div>
+          <TopDonors />
+        </div>
+        <div>
+          <TopDonations />
+          <br />
+        </div>
       </div>
     </React.Fragment>
   );
--- src/shared/Needy/Needy.jsx
+++ src/shared/Needy/Needy.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
+import { BeatLoader } from "react-spinners";
 
 const Needy = () => {
   const [respData, setRespData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const getDonors = async () => {
@@ -13,6 +15,7 @@ const Needy = () => {
       );
       const responseData = await response.json();
       setRespData(responseData.data);
+      setLoading(true);
     };
 
     getDonors();
@@ -21,26 +24,38 @@ const Needy = () => {
   return (
     <Container>
       <h1 style={{ marginTop: "2rem" }}>List of Needy User</h1>
-      <Row>
-        {respData.map((item, index) => (
-          <Card
-            bg="dark"
-            style={{
-              width: "15rem",
-              color: "white",
-              marginRight: "1rem",
-              marginTop: "2rem",
-              marginBottom: "5rem",
-            }}
-            key={index + 1}
-          >
-            <Card.Img variant="top" src={item.photo} />
-            <Card.Body>
-              <Card.Title>{item.name}</Card.Title>
-            </Card.Body>
-          </Card>
-        ))}
-      </Row>
+      {loading ? (
+        <Row>
+          {respData.map((item, index) => (
+            <Card
+              bg="dark"
+              style={{
+                width: "15rem",
+                color: "white",
+                marginRight: "1rem",
+                marginTop: "2rem",
+                marginBottom: "5rem",
+              }}
+              key={index + 1}
+            >
+              <Card.Img variant="top" src={item.photo} />
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+              </Card.Body>
+            </Card>
+          ))}
+        </Row>
+      ) : (
+        <BeatLoader
+          color="black"
+          cssOverride={{
+            width: "60px",
+            height: "50px",
+            margin: "0 auto",
+            marginTop: "10%",
+          }}
+        />
+      )}
     </Container>
   );
 };
